51296fb3927b6767bf07f69a333a74961567e194
MeronAberham <meronaberham36@gmail.com>
2022-06-24 20:08:10 +0100

    first commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,21 @@
-#include <stdio.h>
+#include <stdio.h>
 
 /**
-* main - prints all arguments it received
-*@argc: argument count
-*@argv: argument vector
-*Return: returns 0
-*/
+ * main - prints the number of arguments passed into the program
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-		int i;
+int i = 0;
 
-		for (i = 0; i < argc; i++)
-			printf("%s\n", argv[i]);
+while (argc--)
+{
+	printf("%s\n", argv[i]);
+	i++;
+}
 
-		return (0);
+return (0);
 }
